fbf5fbfc48f4aaff61bcd83afa9d01c10f703bac
HARENI-07 <hareni.priya.irulan@accenture.com>
2025-09-26 10:58:34 +0530

    Update HomeController.java

--- src/main/java/com/springgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/springgeek/helloworld/controller/HomeController.java
@@ -1,16 +1,13 @@
-package
-com.springgeek.helloworld.controller;
+package com.springgeek.helloworld.controller;
 
-import
-org.springframework.web.bind.annotation.GetMapping;
-import
-org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public clas HomeController {
+public class HomeController {
 
-  @GetMApping("/")
-  public String home() {
+  @RequestMapping("/")
+  String home() {
     return "Hello from Hareni!";
   }
 }
